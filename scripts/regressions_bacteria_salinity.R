fd80b41860e5ef7118e26b5ce4aac60364f926ca
bcavazzin <86983937+bcavazzin@users.noreply.github.com>
2023-02-15 17:07:57 +0000

    freq and baye pool mod

--- scripts/regressions_bacteria_salinity.R
+++ scripts/regressions_bacteria_salinity.R
@@ -2,6 +2,7 @@
 # regressions
 # bacteria measurement against salinity 
 
+# libraries ####
 library(dplyr)
 library(broom)
 library(rstan)
@@ -17,6 +18,7 @@ library(modelr)
 library(ggdist)
 library(magrittr)
 library(tibble)
+library(tidyverse)
 
 # dataset ####
 
@@ -31,15 +33,23 @@ dat$log_salinity <- log(dat$Wsalinity)
 dat$log_WpH <- log(dat$WpH)
 dat$log_lakeArea <- log(dat$`Lake area`)
 
-dat <- dat[,-c(8,10,14)]
+dat.2 <- dat[,-c(8,10,14)]
 
-## bacteria vs sal
-dat.2 <- dat %>%
+dat.2 <- dat.2 %>%
   pivot_longer(IIIa:Ic, names_to = "bacteria", values_to = "intensity")
-dat.2 <- dat.2[dat.2$intensity != 0, ]
+dat.2 <- dat.2[dat.2$intensity != 0, ] #remove lakes with signal intensity = 0
 
 dat.2$log_intensity <- log(dat.2$intensity)
 dat.2$bacteria <- as.factor(dat.2$bacteria)
+dat.2 <- dat.2[, c("bacteria", "log_salinity", "log_intensity")]
+
+p1 <- ggplot(dat.2, aes(x = log_salinity, y = log_intensity)) +
+  geom_point() +
+  geom_smooth(method = lm) +
+  facet_wrap(vars(bacteria), ncol = 4) +
+  labs(x = "Salinity (log)", y = "Signal intensity (log)") +
+  theme_bw()
+p1
 
 ############################
 # basic plots
@@ -48,65 +58,32 @@ dat.2$bacteria <- as.factor(dat.2$bacteria)
 # GGally::ggpairs(dat, columns = 20:34)
 
 
-################
-# normal fits
+## Frequentist no pool, pool, partial pool #################
 
-lm_null <- stan_glm(log_intensity ~ 1,
-                    family = "gaussian",
-                    data = dat.2)
+names_bacteria <- levels(dat.2$bacteria) 
 
-yrep_lm_null <- posterior_predict(lm_null)
+m_no_pooling <- list()
 
-lm11 <- stan_glmer(log_intensity ~ log_salinity + (log_salinity|bacteria),
-                 family = "gaussian",
-                 data = dat.2)
-
-yrep_lm11 <- posterior_predict(lm11)
-
-lm12 <- stan_glmer(log_intensity ~ 1 + (1 |bacteria),
-                   family = "gaussian",
-                   data = dat.2)
-
-yrep_lm12 <- posterior_predict(lm12)
-
-##########
-# plots
-
-color_scheme_set("brightblue")
-ppc_dens_overlay(dat.2$log_intensity, yrep_lm11[1:50, ])
-ppc_dens_overlay(dat.2$log_intensity, yrep_lm_null[1:50, ])
-ppc_dens_overlay(dat.2$log_intensity, yrep_lm12[1:50, ])
-
-ppc_dens_overlay(dat.2$log_intensity, exp(yrep_lm11[1:100, ]))
-ppc_dens_overlay(dat.2$log_intensity, exp(yrep_lm_null[1:100, ])) 
-
-plot(dat.2$log_salinity, dat.2$log_intensity)
-abline(coef(lm11)[1], coef(lm11)[2])
-
-# frequentist
-
-freq_lm <- lmer(log_intensity ~ log_salinity + (log_salinity|bacteria),
-              data = dat.2)
-
-freq_lm_pred <- predict(freq_lm)
-
-###################
-# 
-m_no_pooling <- lmList(log_intensity ~ log_salinity | bacteria, dat.2) 
+for (i in names_bacteria) {
+  m_no_pooling[[i]] <- lm(log_intensity ~ log_salinity, filter(dat.2, bacteria == i)) 
+}
 
 df_no_pooling <- tibble(
   Model = "No pooling",
-  Subject = unique(dat.2$bacteria),
-  Intercept = coef(m_no_pooling)[1], 
-  Slope_Salinity = coef(m_no_pooling)[2]
+  bacteria = levels(dat.2$bacteria),
+  Intercept = sapply(m_no_pooling, coef)[1, ], 
+  Slope_Salinity = sapply(m_no_pooling, coef)[2, ]
 )
+
+#write.csv(df_no_pooling, file = "df_no_pool.csv")
+#read.csv("df_no_pool.csv", row.names = 1)
 
 m_pooled <- lm(log_intensity ~ log_salinity, dat.2) 
 
 # Repeat the intercept and slope terms for each participant
 df_pooled <- tibble(
   Model = "Complete pooling",
-  Subject = unique(dat.2$bacteria),
+  bacteria = levels(dat.2$bacteria),
   Intercept = coef(m_pooled)[1], 
   Slope_Salinity = coef(m_pooled)[2]
 )
@@ -114,25 +91,14 @@ df_pooled <- tibble(
 head(df_pooled)
 
 # join raw data
-df_models <- bind_rows(df_pooled, df_no_pooling) %>% 
-  left_join(dat.2, by = "bacteria")
+df_models.1 <- bind_rows(df_pooled, df_no_pooling) 
 
-p_model_comparison <- ggplot(df_models) + 
-  aes(x = Days, y = Reaction) + 
-  # Set the color mapping in this layer so the points don't get a color
-  geom_abline(
-    aes(intercept = Intercept, slope = Slope_Days, color = Model),
-    size = .75
-  ) + 
-  geom_point() +
-  facet_wrap("Subject") +
-  labs(x = xlab, y = ylab) + 
-  scale_x_continuous(breaks = 0:4 * 2) + 
-  # Fix the color palette 
-  scale_color_brewer(palette = "Dark2") + 
-  theme(legend.position = "top", legend.justification = "left")
+ggplot(df_models.1) +
+  facet_wrap("bacteria") +
+  geom_point(data = dat.2, aes(x = log_salinity, y = log_intensity), inherit.aes = FALSE) +
+  geom_abline(aes(intercept = Intercept, slope = Slope_Salinity, color = Model), size = 0.75) +
+  theme_bw()
 
-p_model_comparison
 
 mod.1 <- lmer(log_intensity ~ 1 + log_salinity + (1 + log_salinity | bacteria), dat.2)
 mod.1
@@ -145,59 +111,197 @@ df_partial_pooling <- coef(mod.1)[["bacteria"]] %>%
 
 head(df_partial_pooling)
 
-p_mod.1 <- ggplot(df_partial_pooling) + 
-  aes(x = log_salinity, y = log_intensity) + 
-  # Set the color mapping in this layer so the points don't get a color
-  geom_abline(
-    aes(intercept = Intercept, slope = Slope_Salinity),
-    size = .75
-  ) + 
-  geom_point() +
+df_models.2 <- bind_rows(df_models.1, df_partial_pooling) 
+
+ggplot(df_models.2) +
   facet_wrap("bacteria") +
-  #labs(x = Salinity (Log), y = Bacteria (log)) + 
-  #scale_x_continuous() + 
-  # Fix the color palette 
-  scale_color_brewer(palette = "Dark2") + 
-  theme(legend.position = "top", legend.justification = "left")
+  geom_point(data = dat.2, aes(x = log_salinity, y = log_intensity), inherit.aes = FALSE) +
+  geom_abline(aes(intercept = Intercept, slope = Slope_Salinity, color = Model), size = 0.75) +
+  theme_bw()
 
-p_mod.1 
+## Bayesian no pool, pool, partial pool####################
 
-######################
-# negative binomial
 
-fit_nb0 <- brm(bac ~ 1,
-               family = negbinomial(),
-               data = dat)
-# full model
-fit2 <- brm(bac ~ log_salinity + WT + MAT + log_lakeArea + log_WpH,
-            family = negbinomial(),
-            data = dat)
+df_pulled <- bind_rows(df_no_pooling, df_partial_pooling)
 
-fit3 <- brm(bac ~ log_salinity,
-            family = zero_inflated_negbinomial(),
-            data = dat)
+b <- stan_glmer(
+  log_intensity ~ log_salinity + (log_salinity | bacteria),
+  family = gaussian(),
+  data = dat.2,
+  # prior = normal(0, 2, autoscale = TRUE),
+  # prior_intercept = normal(0, 5, autoscale = TRUE),
+  # prior_covariance = decov(regularization = 2),
+  # prior_aux = cauchy(0, 1, autoscale = TRUE),
+  seed = 20211116
+)
 
-fit2
-fit3
+b
+xx <- extract(b)$stan_summary
 
-yrep_nb0 <- posterior_predict(fit_nb0)
-yrep_fit2 <- posterior_predict(fit2)
-yrep_fit3 <- posterior_predict(fit3)
+df_posterior <- b %>% 
+  as.data.frame() %>% 
+  as_tibble()
 
-epred_nb0 <- posterior_epred(fit_nb0)
-epred_fit2 <- posterior_epred(fit2)
-epred_fit3 <- posterior_epred(fit3)
+## Average intercept and slope
+ggplot(df_posterior) + 
+  aes(x = `(Intercept)`, y = `log_salinity`) + 
+  # Calculate the density
+  stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
+  ggtitle("Where's the average intercept and slope?") + 
+  xlab("Estimate for average intercept") + 
+  ylab("Estimate for average slope") +
+  # Use the same coordinate limits as last plot
+  coord_cartesian(
+    xlim = range(df_pulled$Intercept), 
+    ylim = range(df_pulled$Slope_Salinity),
+    expand = TRUE
+  ) + 
+  guides(fill = "none")
 
-ppc_dens_overlay(dat$bac, yrep_fit2[1:100, ])
-ppc_dens_overlay(dat$bac, yrep_fit3[1:100, ])
-ppc_dens_overlay(dat$bac, yrep_nb0[1:100, ])
 
-ppc_dens_overlay(dat$log_bac, log(yrep_fit2[1:100, ]))
-ppc_dens_overlay(dat$log_bac, log(yrep_fit3[1:100, ]))
+df_effects <- df_posterior %>%
+  mutate(
+    # Find all the columns with the pattern "b[(Intercept". Add the column
+    # `(Intercept)` to each of those columns.
+    across(
+      .cols = matches("b\\[\\(Intercept"), 
+      .fns = ~ . + `(Intercept)`
+    ),
+    # Again for slope
+    across(
+      .cols = matches("b\\[log_salinity"), 
+      .fns = ~ . + log_salinity
+    )
+  )
 
-ppc_dens_overlay(dat$bac, epred_fit2[1:100, ]) + xlim(0, 800)
-ppc_dens_overlay(dat$bac, epred_fit3[1:100, ]) + xlim(0, 800)
-ppc_dens_overlay(dat$bac, epred_nb0[1:100, ]) + xlim(0, 800)
+# Convert to a long format
+df_long_effects <- df_effects %>%
+  select(matches("b\\[")) %>%
+  rowid_to_column("draw") %>%
+  tidyr::pivot_longer(
+    cols = c(-draw),
+    # when we make new columns with pivot_ functions, the
+    # they get quotes
+    names_to = "Parameter", 
+    values_to = "Value"
+  )
 
-ppc_dens_overlay(dat$log_bac, log(epred_fit2[1:100, ]))
+# df_long_effects <-
+#   df_long_effects %>% 
+#   mutate(
+#     Effect = Parameter %>% 
+#       stringr::str_detect("Intercept") %>%
+#       ifelse(., "Intercept", "Slope_Salinity"),
+#     Subject = Parameter %>%
+#       stringr::str_extract("\\d\\d\\d")
+#   ) %>% 
+#   select(draw, bacteria, Effect, Value)
 
+df_long_effects2 <-
+  df_long_effects %>%
+tidyr::separate_wider_delim(Parameter,
+                            delim = " ", names = c("param", "bacteria")) %>% 
+  mutate(bacteria = gsub("bacteria:", "", bacteria),
+         bacteria = gsub("]", "", bacteria),
+         param = gsub("b\\[", "", param),
+         param = gsub("\\(", "", param),
+         param = gsub(")", "", param))
+
+df_long_effects2
+
+set.seed(20220330)
+
+df_samples <- df_long_effects2 %>%
+  filter(draw %in% sample(1:4000, size = 50)) %>%
+  tidyr::pivot_wider(names_from = param, values_from = Value)
+
+df_samples
+
+ggplot(df_samples) +
+  facet_wrap("bacteria") +
+  geom_point(data = dat.2, aes(x = log_salinity, y = log_intensity), inherit.aes = FALSE) +
+  geom_abline(aes(intercept = Intercept, slope = log_salinity), size = 0.75,
+              color = "#3366FF", 
+              alpha = .1)
+
+b %>%
+  spread_draws(`(Intercept)`, b[,group]) %>%
+  median_qi(condition_mean = `(Intercept)` + b, .width = c(.95, .8, .5))
+
+
+# forest plot intercept
+p1 <- b %>%
+  spread_draws(`(Intercept)`, b[,group]) %>%
+  median_qi(condition_mean = `(Intercept)` + b, .width = c(.95, .66)) %>%
+  add_row(group = "Pulled Interc", condition_mean = 11.2, .lower = 11.00, 
+          .upper = 11.33, .width = 0.66, 
+          .point = "median", .interval = "qi") %>%
+  add_row(group = "Pulled Interc", condition_mean = 11.2, .lower = 10.67, 
+          .upper = 11.70, .width = 0.95, 
+          .point = "median", .interval = "qi") %>%
+  ggplot(aes(y = group, x = condition_mean, xmin = .lower, xmax = .upper)) +
+  geom_pointinterval() +
+  labs(x ="Mean Intercept", y = "") +
+  scale_y_discrete(labels = c("Ia", "Ib", "Ic", "IIa", "IIb",
+                              "IIb'", "IIc", "IIIa", "IIIa'",
+                              "IIIb", "IIIb'", "IIIc","Model av.")) + # "IIa'", "IIc'", "IIIc'"
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"))
+
+# forest plot salinity slope
+p2 <- b %>%
+  spread_draws(`log_salinity`, b[,group]) %>%
+  median_qi(condition_mean = `log_salinity` + b, .width = c(.95, .66)) %>%
+  add_row(group = "Pulled slope", condition_mean = -0.453, .lower = -0.511, 
+          .upper = -0.396, .width = 0.66, 
+          .point = "median", .interval = "qi") %>%
+  add_row(group = "Pulled slope", condition_mean = -0.453, .lower = -0.634, 
+          .upper = -0.283, .width = 0.95, 
+          .point = "median", .interval = "qi") %>%
+  ggplot(aes(y = group, x = condition_mean, xmin = .lower, xmax = .upper)) +
+  geom_pointinterval()  +
+  labs(x ="Mean Slope", y = "") +
+  scale_y_discrete(labels = c("Ia", "Ib", "Ic", "IIa", "IIb",
+                              "IIb'", "IIc", "IIIa", "IIIa'",
+                              "IIIb", "IIIb'", "IIIc","Model av.")) + # "IIa'", "IIc'", "IIIc'"
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"))
+
+#library(ggpubr)
+ggarrange(p1, p2,
+          ncol = 2, nrow = 1)
+
+## stan_glm OLD ##############
+
+lm_null <- stan_glm(log_intensity ~ 1,
+                    family = "gaussian",
+                    data = dat.2)
+
+yrep_lm_null <- posterior_predict(lm_null)
+
+lm11 <- stan_glmer(log_intensity ~ log_salinity + (log_salinity|bacteria),
+                   family = "gaussian",
+                   data = dat.2)
+
+yrep_lm11 <- posterior_predict(lm11)
+
+# plots
+
+color_scheme_set("brightblue")
+ppc_dens_overlay(dat.2$log_intensity, yrep_lm11[1:50, ])
+ppc_dens_overlay(dat.2$log_intensity, yrep_lm_null[1:50, ])
+
+ppc_dens_overlay(dat.2$log_intensity, exp(yrep_lm11[1:100, ]))
+ppc_dens_overlay(dat.2$log_intensity, exp(yrep_lm_null[1:100, ])) 
+
+plot(dat.2$log_salinity, dat.2$log_intensity)
+abline(coef(lm11)[1], coef(lm11)[2])
+
+# frequentist
+
+freq_lm <- lmer(log_intensity ~ log_salinity + (log_salinity|bacteria),
+                data = dat.2)
+
+freq_lm_pred <- predict(freq_lm)
