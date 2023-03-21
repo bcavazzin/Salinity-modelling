1083c2f9993fa1fefd0b98f0188ad3024fe9b9ed
bcavazzin <86983937+bcavazzin@users.noreply.github.com>
2023-03-21 10:14:45 +0000

    MAT ph

--- scripts/regression_bacteria_MAT_pH.R
+++ scripts/regression_bacteria_MAT_pH.R
@@ -30,7 +30,7 @@ dat <- dat[dat$Wsalinity != 0, ]
 dat$log_salinity <- log(dat$Wsalinity)
 dat$log_WpH <- scale(log(dat$WpH), scale = FALSE)
 dat$log_lakeArea <- log(dat$`Lake area`)
-dat$MAT <- scale(dat$MAT, scale = FALSE)
+#dat$MAT <- scale(dat$MAT, scale = FALSE)
 dat$Soi_pH <- scale(dat$Soi_pH, scale = FALSE) 
 
 dat.2 <- dat
@@ -44,7 +44,8 @@ dat.2 <- dat.2[dat.2$intensity != 0, ] #remove  with signal intensity = 0
 dat.2$log_intensity <- log(dat.2$intensity)
 dat.2$bacteria <- as.factor(dat.2$bacteria)
 #dat.3 <- dat.2[, c("LakeName", "bacteria", "log_salinity", "log_intensity")] #for Section 3
-dat.2 <- dat.2[, c("bacteria", "log_salinity", "log_intensity", "MAT", "log_WpH", "Soi_pH", "IR6")] #for section 1 and 2
+dat.2 <- dat.2[, c("bacteria", "log_salinity", "log_intensity", "MAT", 
+                   "log_WpH", "Soi_pH", "IR6", "MBT5ME", "SpringT", "SummerT")] #for section 1 and 2
 
 # regression + plots ####
 
@@ -88,13 +89,41 @@ p.IR6ME <- ggplot(dat.2, aes(x = log_salinity, y = IR6)) +
 p.IR6ME
 
 
-p.MBT <- ggplot(dat.2, aes(x = MAT, y = log_intensity)) +
+p.MBT <- ggplot(dat.2, aes(x = MAT, y = MBT5ME)) +
   geom_point() +
   geom_smooth(method = lm) +
-  facet_wrap(vars(bacteria), ncol = 4) +
-  labs(x = "MAT", y = "Signal intensity (log)") +
+  labs(x = "MAT", y = "MBT'5Me") +
   stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
            p.accuracy = 0.001, r.accuracy = 0.01) + 
   theme_bw()
 p.MBT
+
+p.MBT <- ggplot(dat.2, aes(x = MAT, y = MBT5ME)) +
+  geom_point() +
+  geom_smooth(method = lm) +
+  labs(x = "MAT", y = "MBT'5Me") +
+  stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+           p.accuracy = 0.001, r.accuracy = 0.01) + 
+  theme_bw()
+p.MBT
+
+p.MBT.spring <- ggplot(dat.2, aes(x = SpringT, y = MBT5ME)) +
+  geom_point() +
+  geom_smooth(method = lm) +
+  labs(x = "Spring air temperature", y = "MBT'5Me") +
+  stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+           p.accuracy = 0.001, r.accuracy = 0.01) + 
+  theme_bw()
+p.MBT.spring
+
+p.MBT.summer <- ggplot(dat.2, aes(x = SummerT, y = MBT5ME)) +
+  geom_point() +
+  geom_smooth(method = lm) +
+  labs(x = "Summer air temperature", y = "MBT'5Me") +
+  stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+           p.accuracy = 0.001, r.accuracy = 0.01) + 
+  theme_bw()
+p.MBT.summer
+
+
 
