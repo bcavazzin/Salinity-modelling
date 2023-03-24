2be910aef4f37daf3e32dc6dfbde61e3a9a384f8
bcavazzin <86983937+bcavazzin@users.noreply.github.com>
2023-03-24 17:23:01 +0000

    BUGS still not working

--- scripts/main_script_lm_long_missing.R
+++ scripts/main_script_lm_long_missing.R
@@ -20,10 +20,9 @@ dat_intens <- read.csv(here::here("raw data", "data_signal_intensity.csv"),
                 check.names = FALSE, header = TRUE)
 dat_intens <- dat_intens[dat_intens$Wsalinity != 0, ]
 
-
 # jags set-up ####
 
-filein <- "BUGS/model_missing_lm_long.txt"
+filein <- "BUGS/model_missing_lm_long2.txt"
 params <- c("alpha", "beta", "mu_alpha", "sd_alpha", "mu_beta",
             "sd_beta", "missing", "log_missing", "mu.x", "p.x", "log_salinity")#,
             #  "pred_mean_lsalinity", #"pred_lsalinity")
@@ -34,7 +33,7 @@ n.burnin <- 1000
 n.thin <- floor((n.iter - n.burnin)/500)
 n_dat <- nrow(dat_long)
 
-missing_lake_id <- 935
+missing_lake_id <- 861
 
 missing_lake_dat <-
   filter(dat_long, LakeID == missing_lake_id) |> 
@@ -52,6 +51,10 @@ bac_names <- levels(dat_total$bacteria)
 lakeIDs <- sort(unique(dat_total$LakeID))
 n_lakes <- length(lakeIDs)
 
+sal_dat <- dat_intens[order(dat_intens$LakeID),]
+salinity_dat <- log(sal_dat$Wsalinity)
+salinity_dat <- append(salinity_dat,'NA')
+
 intens_mat <- dat_intens %>%
   select("LakeID", "IIIa":"Ic") %>% 
   mutate(across(everything(), ~replace(., . == 0, NA)),
@@ -66,14 +69,15 @@ intens_mat[n_lakes, ] <- intens_mat[which(lakeIDs == missing_lake_id), ]
 dataJags <-
   list(bac_id = as.numeric(as.factor(dat_total$bacteria)),
        n_bac = length(bac_names),
-       log_salinity = dat_total$log_salinity,
+       log_salinity = salinity_dat, #dat_total$log_salinity
        lake = as.numeric(as.factor(dat_total$LakeID)),
        intensity = intens_mat,
        n_dat = n_dat + n_missing_dat,
        n_obs = n_dat,
        n_miss = n_missing_dat,
        n_lake_miss = n_missing_lake,
-       n_lake_obs = n_lakes)
+       n_lake = n_lakes,
+       n_lake_obs = n_lakes-n_missing_lake)
 
 res_bugs <-
   jags(data = dataJags,
